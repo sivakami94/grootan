8345435ca7700e240f9652106572614a300f0643
unknown <abisivakami91@gmail.com>
2021-06-15 12:39:13 +0530

    first

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express=require("express")
-const app=express();
+const express = require("express")
+const app = express();
 const axios = require('axios')
 const fs = require('fs');
 const path = require('path');
-const expressLayout=require("express-ejs-layouts")
-const session=require("express-session")
-const baseurl="https://userdetails.free.beeceptor.com";
+const expressLayout = require("express-ejs-layouts")
+const session = require("express-session")
+const baseurl = "https://userdetails.free.beeceptor.com";
 app.use(express.static("public"))
 app.use(expressLayout)
 app.set('view engine', 'ejs')
@@ -15,10 +15,10 @@ app.use(express.json())
 
 
 app.use(session({
-    secret:"GrootanTask",
-    resave:true,
-    saveUninitialized:true,
-    
+    secret: "GrootanTask",
+    resave: true,
+    saveUninitialized: true,
+
 }))
 
 const isLoggedIn = function (req, res, next) {
@@ -28,24 +28,24 @@ const isLoggedIn = function (req, res, next) {
         res.redirect('/login')
 }
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.curuser = req.session.user;
     next();
 })
 
-app.get("/",isLoggedIn ,(req,res)=>{
+app.get("/", isLoggedIn, (req, res) => {
     res.render("user");
 })
 
 
 //mock API, which will return a list of users.
-app.get('/users', isLoggedIn,async (req, res) => {
-    
+app.get('/users', isLoggedIn, async (req, res) => {
+
     const users = await axios.get("https://userdetails.free.beeceptor.com/users")
-    
+
     // let rawdata = fs.readFileSync(path.resolve(__dirname, 'user.json'));
     //  let users = JSON.parse(rawdata);
-     
+
     res.json(users.data.data)
 })
 
@@ -71,13 +71,12 @@ app.get('/logout', (req, res) => {
 
 app.get('/show/:mailid/', isLoggedIn, async (req, res) => {
     const users = await axios.get('https://userdetails.free.beeceptor.com/users')
-    
     // let rawdata = fs.readFileSync(path.resolve(__dirname, 'user.json'));
     //  let users = JSON.parse(rawdata);
     var index = users.data.data.map(u => u.email)
-    res.render('show', {user: users.data.data[index.indexOf(req.params.mailid)]})
+    res.render('show', { user: users.data.data[index.indexOf(req.params.mailid)] })
 })
 
-app.listen(process.env.PORT||4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log("server stated");
 })
